d3e3d6240af37a4fd3d13980cc7f060966330430
Xalorian <mustafakapasi401@gmail.com>
2026-03-22 14:56:57 +0530

    Create simplecalc.py

---
+++ level2/simplecalc.py
@@ -0,0 +1,23 @@
+while True:
+    try:
+        num1 = float(input("enter your first number : "))
+        num2 = float(input("enter your second number : "))
+        operation = input("give operation +,-,/,*,%,**  : ")
+        if operation == "+":
+            print(f"{num1} + {num2} = {num1+num2}")
+        elif operation == "-":
+            print(f"{num1} - {num2} = {num1-num2}")
+        elif operation == "/":
+            if num2 == 0:
+                print("number cannot be divisible by 0")
+            else:
+                print(f"{num1} * {num2} = {num1/num2}")
+        elif operation == "*":
+            print(f"{num1} * {num2} = {num1*num2}")
+        else :
+            print("Invaild operation")
+        choice = input("Should we countinue ? type (yes/no) : ")
+        if choice == "no":
+            break
+    except ValueError:
+        print("invaild input")
