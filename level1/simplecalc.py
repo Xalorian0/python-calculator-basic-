b50b77f50e64c0c394a8e06027b34636421e11aa
Xalorian <mustafakapasi401@gmail.com>
2026-03-22 14:50:50 +0530

    Add simple calculator functionality in simplecalc.py

---
+++ level1/simplecalc.py
@@ -0,0 +1,16 @@
+try:
+    num1 = float(input("enter your first number : "))
+    num2 = float(input("enter your second number : "))
+    operation = input("give operation +,-,/,*  : ")
+    if operation == "+":
+        print(num1+num2)
+    elif operation == "-":
+        print(num1-num2)
+    elif operation == "/":
+        print(num1/num2)
+    elif operation == "*":
+        print(num1*num2)
+    else:
+        print("invaild output")
+except:
+    print("invaild output")
