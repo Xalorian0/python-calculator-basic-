f66252f6445a10f225a04b398b251a4c079627c7
Cups Studio <mr.man8man@gmail.com>
2026-05-01 14:46:19 +0200

    Update calculator.py

--- level3/calculator.py
+++ level3/calculator.py
@@ -29,8 +29,8 @@ def divide(a,b):
         return(a/b)
 def multiply(a,b):
     return(a*b)
-def Power(a):
-    return(a ** a)
+def Power(a, b):
+    return(a ** b)
 def perc(a,b):
     return(a * b)/100
 
